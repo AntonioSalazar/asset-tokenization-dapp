66a36aea2e4b9f724d5e55752b007853989248e5
unknown <antonio.salazar.emrsn@gmail.com>
2021-04-15 23:25:11 -0500

    all tests are passing now

--- test/ASDToken.test.js
+++ test/ASDToken.test.js
@@ -11,16 +11,34 @@ let chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 
 const expect = chai.expect;
-
-contract('Token tests', async accounts => {
-
-    const [deployerAccount, recipiect , thirdAccount ] = accounts;
-
-    it('All tokens should be in owner account', async () => {
-        let instance = await ASDToken.deployed();
-        let totalSupply = await instance.totalSupply();
-        expect( instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
-    })
-
-
-});
+let instance;
+let totalSupply;
+ 
+contract("Test UpStateToken", async (accounts) => {
+  beforeEach(async () => {
+    instance = await ASDToken.deployed();
+    totalSupply = await instance.totalSupply();
+  });
+ 
+  it("Initial Supply assigned to the owner account", async () => {
+    expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(
+      totalSupply
+    );
+  });
+ 
+  it("Transfer tokens", async () => {
+    await instance.transfer(accounts[1], 100);
+    expect(instance.balanceOf(accounts[1])).to.eventually.be.a.bignumber.equal(
+      new BN(100)
+    );
+  });
+ 
+  it("Should not be able to spend more than available balance", async () => {
+    let availableBal = await instance.balanceOf(accounts[0]);
+    try {
+      await instance.transfer(accounts[1], availableBal);
+    } catch (err) {
+      assert(err);
+    }
+  });
+})
