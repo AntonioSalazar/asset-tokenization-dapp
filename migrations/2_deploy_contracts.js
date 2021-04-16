d6f5a03680fcc55b2fc8c294f2c9b8978bfdbc26
unknown <antonio.salazar.emrsn@gmail.com>
2021-04-16 14:13:27 -0500

    added kyc contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
 var ASDToken = artifacts.require("./ASDToken.sol");
 var ASDTokenCrowdsale = artifacts.require("./ASDTokenCrowdsale");
+var KycContract = artifacts.require('./KycContract');
 require('dotenv').config({path: "../.env"});
 
 module.exports = async function(deployer) {
   let addr = await web3.eth.getAccounts();
   await deployer.deploy(ASDToken, process.env.INITIAL_TOKENS);
-  await deployer.deploy(ASDTokenCrowdsale, 1, addr[0], ASDToken.address);
+  await deployer.deploy(KycContract);
+  await deployer.deploy(ASDTokenCrowdsale, 1, addr[0], ASDToken.address, KycContract.address);
   let instance = await ASDToken.deployed();
   await instance.transfer(ASDTokenCrowdsale.address, process.env.INITIAL_TOKENS);
 };
