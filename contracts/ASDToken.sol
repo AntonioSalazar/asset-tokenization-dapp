46ddd0f24e022496c7c8ce120c6519739b643bbf
unknown <antonio.salazar.emrsn@gmail.com>
2021-04-16 11:51:43 -0500

    Added a Crowdsale contract from OpenZeppelin also added ASDTokenCrowdsale contract which be holding all of the tokens

--- contracts/ASDToken.sol
+++ contracts/ASDToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.6.1;
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ASDToken is ERC20 {
-    constructor(uint256 initialSupply)   ERC20  ("Antonio Salazar Developer Token", "ASDT") {
+    constructor(uint256 initialSupply) public  ERC20  () {
         _mint(msg.sender, initialSupply);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var ASDToken = artifacts.require("./ASDToken.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(ASDToken, 10000);
+var ASDTokenCrowdsale = artifacts.require("./ASDTokenCrowdsale");
+module.exports = async function(deployer) {
+  let addr = await web3.eth.getAccounts();
+  await deployer.deploy(ASDToken, 1000000);
+  await deployer.deploy(ASDTokenCrowdsale, 1, addr[0], ASDToken.address);
+  let instance = await ASDToken.deployed();
+  await instance.transfer(ASDTokenCrowdsale.address, 1000000);
 };
