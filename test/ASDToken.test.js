3b89facb648379bc70579feef382e18e20fede47
unknown <antonio.salazar.emrsn@gmail.com>
2021-04-15 21:47:24 -0500

    test to create tokens and all of them to be in the caller account is ready

--- test/ASDToken.test.js
+++ test/ASDToken.test.js
@@ -1,1 +1,26 @@
-const ASDToken = artifacts.require('ASDToken')
+const ASDToken = artifacts.require('ASDToken');
+
+let chai = require('chai');
+const BN = web3.utils.BN;
+console.log(BN);
+
+const chaiBN = require('chai-bn')(BN);
+chai.use(chaiBN);
+
+let chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+contract('Token tests', async accounts => {
+
+    const [deployerAccount, recipiect , thirdAccount ] = accounts;
+
+    it('All tokens should be in owner account', async () => {
+        let instance = await ASDToken.deployed();
+        let totalSupply = await instance.totalSupply();
+        expect( instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+    })
+
+
+});
