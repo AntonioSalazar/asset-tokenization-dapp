9cc963c3b42b6fe24c4b3954613dc0f55d38fbf4
unknown <antonio.salazar.emrsn@gmail.com>
2021-04-17 00:33:54 -0500

    Able to whitelist addresses

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+require('dotenv').config({path: "./.env"});
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const AccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +10,12 @@ module.exports = {
   networks: {
     develop: {
       port: 7545
+    },
+    ganache_local: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, 'Http://127.0.0.1:7545', AccountIndex)
+      },
+      network_id: 5777
     }
   },
 
